b1f908b2039b28397be5bacb79563eacd1b9e492
Nguyen Thanh Minh <ngthminh.dev@gmail.com>
2023-03-25 22:43:07 +0700

    Vendor: Update version

--- src/main.ts
+++ src/main.ts
@@ -58,7 +58,7 @@ async function bootstrap() {
 
   await app.listen(parseInt(process.env.SERVER_PORT)).then(() => {
     console.log(
-      `Server is running at ${process.env.SERVER_HOST}:${process.env.SERVER_PORT} --version: 0.0.02`,
+      `Server is running at ${process.env.SERVER_HOST}:${process.env.SERVER_PORT} --version: 0.0.03`,
     );
   });
 }

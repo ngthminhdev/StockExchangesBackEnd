0979fe5064f654e829d786b9d60c1caba51001fd
Nguyen Thanh Minh <ngthminh.dev@gmail.com>
2023-04-06 22:16:44 +0700

    Feat: Test get MAC on aws

--- src/middlewares/mac.middleware.ts
+++ src/middlewares/mac.middleware.ts
@@ -2,6 +2,7 @@ import { Injectable, NestMiddleware } from "@nestjs/common";
 import { NextFunction, Response } from "express";
 import { MRequest } from "../types/middleware";
 import { UtilCommonTemplate } from "../utils/utils.common";
+
 const macaddress =  require('macaddress');
 
 @Injectable()
@@ -11,6 +12,8 @@ export class MacMiddleware implements NestMiddleware {
     const realIp: any = req.headers["x-real-ip"] || req.headers["x-forwarded-for"] || req.socket.remoteAddress;
     const macObj = macaddress.networkInterfaces();
     const mac = macObj?.[Object.keys(macObj)[0]]?.mac || 'unknown';
+    console.log({ macObj });
+
 
     req.deviceId = UtilCommonTemplate.generateDeviceId(mac, userAgent, realIp);
     req.realIP = realIp;
